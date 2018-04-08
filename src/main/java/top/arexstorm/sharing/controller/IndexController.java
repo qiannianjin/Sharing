801d043b9b9fbefeddf827b183818750999b64b0
sandiegoe <sandiegoe@126.com>
2018-04-08 21:52:22 +0800

    修改首页显示错误

--- src/main/java/top/arexstorm/sharing/controller/IndexController.java
+++ src/main/java/top/arexstorm/sharing/controller/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
 	@RequestMapping(value="")
 	public String index() {
-		return "/index";
+		return "index";
 	}
 }
--- src/test/java/top/arexstorm/sharing/service/order/OrderServiceTest.java
+++ src/test/java/top/arexstorm/sharing/service/order/OrderServiceTest.java
@@ -1,12 +1,9 @@
 package top.arexstorm.sharing.service.order;
 
-import static org.junit.Assert.fail;
-
 import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.internal.matchers.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
