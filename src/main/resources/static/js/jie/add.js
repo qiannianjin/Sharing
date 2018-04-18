5b12e457052e15e55f622047e2bb2e80393f549e
arex <sandiegoe@126.com>
2018-04-18 19:37:42 +0800

    共享信息发布添加类型成功

--- src/main/resources/static/js/jie/add.js
+++ src/main/resources/static/js/jie/add.js
@@ -7,31 +7,18 @@
 			$.each(list, function(i, n) {
 				var option = $('<option value="' + n.id + '">' + n.name + '</option>');
 				$("select[name=typeid]").append(option);
-				var dd = $('<dd class="" lay-value="' + n.id + '">' + n.name + '</dd>');
-				$("div.layui-unselect dl.layui-anim").append(dd);
+//				var dd = $('<dd class="" lay-value="' + n.id + '">' + n.name + '</dd>');
+//				$("div.layui-unselect dl.layui-anim").append(dd);
 			});
+			
+//			layui.use('form', function(){
+				// 动态添加内容 layui 自动渲染失败，需要手动渲染
+				var form = layui.form;
+			    form.render('select');
+//			});
 		}
 	});
 	
-layui.define('fly', function(exports){
-	var form = layui.form;
-	//监听专栏选择
-	form.on('select(column)', function(obj){
-	    var value = obj.value
-	    ,elemQuiz = $('#LAY_quiz')
-	    ,tips = {
-	      tips: 1
-	      ,maxWidth: 250
-	      ,time: 10000
-	    };
-	    elemQuiz.addClass('layui-hide');
-	    if(value === '0'){
-	      layer.tips('下面的信息将便于您获得更好的答案', obj.othis, tips);
-	      elemQuiz.removeClass('layui-hide');
-	    } else if(value === '99'){
-	      layer.tips('系统会对【分享】类型的帖子予以飞吻奖励，但我们需要审核，通过后方可展示', obj.othis, tips);
-	    }
-	  });
-	})
-	exports('jie', null);
-});
+	
+	
+})
