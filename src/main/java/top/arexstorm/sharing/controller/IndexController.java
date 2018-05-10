a84cf97c93f0d2311725e84fdad7e85a39456528
sandiegoe <sandiegoe@126.com>
2018-05-10 18:05:42 +0800

    准备页面顶部链接页面

--- src/main/java/top/arexstorm/sharing/controller/IndexController.java
+++ src/main/java/top/arexstorm/sharing/controller/IndexController.java
@@ -2,12 +2,26 @@ package top.arexstorm.sharing.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class IndexController {
 
-	@RequestMapping(value="")
+	@RequestMapping(value = "")
 	public String index() {
 		return "index";
 	}
+
+	@RequestMapping(value = "/about")
+	public String about() {
+		return "other/about";
+	}
+
+	@RequestMapping(value = "/communication")
+	public String communication() {
+		return "other/communication";
+	}
+
+    @RequestMapping(value = "/case")
+    public String case1() {
+        return "case/case";
+    }
 }
